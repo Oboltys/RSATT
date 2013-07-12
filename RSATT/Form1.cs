bed11f2cf1ef721d8c53d68b05b227bb97def01d
Pavel <Pavel.Ivanchenko@studentpartner.com>
2013-07-12 14:27:50 +0700

    RSA is an algorithm for public-key cryptography.

---
+++ RSATT/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RSATT
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!(textBox1.Text.Length > 0))
+            {
+                label5.Text = "Введите первую часть ключа для дешифровки!";
+            }
+            else if (!(textBox2.Text.Length > 0))
+            {
+                label5.Text = "Введите вторую часть ключа для дешифровки!";
+            }
+            else if (richTextBox2.Text.Length > 0)
+            {
+                label5.Text = "";
+                
+                RSA rsa = new RSA();
+                richTextBox1.Text = rsa.decode(richTextBox2.Text, textBox1.Text, textBox2.Text);
+            }
+            else
+            {
+                label5.Text = "Текст для шифрования не может быть пустым!";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (richTextBox1.Text.Length > 0)
+            {
+                label5.Text = "";
+                
+                RSA rsa = new RSA();
+                richTextBox2.Text = rsa.encode(richTextBox1.Text);
+                textBox1.Text = "" + rsa.GetNKey();
+                textBox2.Text = "" + rsa.GetDKey();
+            }
+            else
+            {
+                
+                label5.Text = "Текст для шифрования не может быть пустым!";
+            }
+        }
+    }
+}
